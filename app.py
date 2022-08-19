e65f87f48f8a748cb0449e81c6cbfbcacd8382a9
barrymore <plotnikovalex@gmail.com>
2022-08-20 00:02:14 +0300

    Flask Predict

--- app.py
+++ app.py
@@ -2,15 +2,11 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
-
-
-
 @app.route('/predict', methods=['GET'])
 def predict_for_string():
     if request.method == 'GET':
         user_request = request.args.get('user_request')
         return f'ваш запрос = {user_request}'
-
 
 
 if __name__ == '__main__':
--- upload_file.py
+++ upload_file.py
@@ -1,5 +1,4 @@
-import json
-import requests
+import json, requests
 
 filename = input('Путь к файлу json: ')
 list_product = []
